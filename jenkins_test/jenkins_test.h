3d75b6df56103bdb65d3c8ba944ea05e11dd999a
zhangyun <zhangyun@yunzhisheng.cn>
2015-01-04 19:37:49 +0800

    add

---
+++ jenkins_test/jenkins_test.h
@@ -0,0 +1,13 @@
+//
+//  jenkins_test.h
+//  jenkins_test
+//
+//  Created by yunzhisheng-zy on 15-1-4.
+//  Copyright (c) 2015年 usc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface jenkins_test : NSObject
+
+@end
---
+++ jenkins_test/jenkins_test.m
@@ -0,0 +1,13 @@
+//
+//  jenkins_test.m
+//  jenkins_test
+//
+//  Created by yunzhisheng-zy on 15-1-4.
+//  Copyright (c) 2015年 usc. All rights reserved.
+//
+
+#import "jenkins_test.h"
+
+@implementation jenkins_test
+
+@end
